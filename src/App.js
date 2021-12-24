740f826be54f35fca0f5075140dbb01d4074098b
AnastasiyaMAV <mihalukova112@mail.ru>
2021-12-24 10:01:04 +0200

    Добалвение карточек на страницу + пагинация для карточек

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './components/styles/Font.css'
 import './components/styles/Header.css'
 import './components/styles/Footer.css'
 import './components/styles/EditableTable.css'
+import './components/styles/CardWord.css'
 import 'antd/dist/antd.css';
-import { Layout } from 'antd';
+import { Layout, Row, List } from 'antd';
 import EditableTable from './components/EditableTable';
+import CardWord from './components/CardWord';
 
 const { Header, Footer, Content } = Layout;
+const originData = require('./components/JSON/originData.json');
 
 function App() {
   return (
@@ -16,6 +19,27 @@ function App() {
       <Layout>
         <Header className='header'>Header</Header>
         <Content><EditableTable /></Content>
+        <Content>
+          <List
+            grid={{ gutter: 0, column: 4 }}
+            pagination={{
+              onChange: page => {
+                console.log(page);
+              },
+              pageSize: 8,
+            }}
+            dataSource={originData}
+            renderItem={item => (
+              <List.Item>
+                {
+                  <CardWord key={item.id} english={item.english} 
+                    transcription={item.transcription} 
+                    russian={item.russian}/>
+                }
+              </List.Item>
+            )}
+          />,        
+        </Content>
         <Footer className='footer'>Footer</Footer>
       </Layout>
     </div>
---
+++ src/components/CardWord.jsx
@@ -0,0 +1,24 @@
+import { Card, Col, Button } from 'antd';
+import React, { useState } from 'react';
+
+export default function CardWord(props) {
+
+    const [pressed, setPressed] = useState(false);
+
+    const handleChange = () => {
+        setPressed(!pressed);
+    };
+    
+    return(
+
+        <Card hoverable className='cardStyle'>
+            <h1 className='cardEnglishWord'>{props.english}</h1>
+            <h2 className='cardTranscription'>{props.transcription}</h2>
+            <hr/>
+
+            <Button {...props} className='cardButton' onClick={handleChange}>{pressed ? "Translation" : "Check"}</Button>
+            <h1 className={pressed ? 'cardRussianhWordReverse' : 'cardRussianhWordInitial'}>{props.russian}</h1>
+        </Card>
+
+    );
+}
