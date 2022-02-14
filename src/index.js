c0b1bc7719ca2ada11ed023c4da3a83cc421ba8a
AnastasiyaMAV <mihalukova112@mail.ru>
2022-02-14 20:38:09 +0200

    HashRouter

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <HashRouter basename={process.env.PUBLIC_URL}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </HashRouter>,
+
   document.getElementById('root')
 );
 
